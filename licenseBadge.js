b2b3acdafbb37ca67bd00f8a2bd065c5fb71a5f3
Neeko623 <74262676+Neeko623@users.noreply.github.com>
2021-02-06 15:45:11 -0500

    Delete licenseBadge.js

--- licenseBadge.js
+++
@@ -1,7 +0,0 @@
-const licenseBadge = {
-    'ISC':'[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)',
-    'MIT':'[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)',
-    'The Unlicense':'[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)'
-}
-
-module.exports = licenseBadge;
