d42ed2771fa4da6ad2dd949cc81d0901533a175c
Neeko Tang <neeko_tvxq@hotmail.com>
2021-02-06 15:40:06 -0500

    update badge code

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -1,10 +1,36 @@
-const licenseBadge = require('/Users/neekotang/Bootcamp/README-Generator/licenseBadge.js');
+// // TODO: Create a function that returns a license badge based on which license is passed in
+// // If there is no license, return an empty string
+// function renderLicenseBadge(license) {
+//   if (license == 'None') { return '';}
+//   else if (license == 'ISC') {return `![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)`;}
+//   else if (license == 'MIT') {return `![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)`;}
+//   else if (license == 'The Unlicense') {return `![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)`;}
+// }
+// // TODO: Create a function that returns the license link
+// // If there is no license, return an empty string
+// function renderLicenseLink(license) {
+//   if (license = 'None') {return '';}
+//   else if (license == 'ISC') {return `(https://opensource.org/licenses/ISC)`;}
+//   else if (license == 'MIT') {return `(https://opensource.org/licenses/MIT)`;}
+//   else if (license == 'The Unlicense') {return `(https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)`;}
+// }
 
-// function to generate markdown for README
+// // TODO: Create a function that returns the license section of README
+// // If there is no license, return an empty string
+// function renderLicenseSection(license) {
+//   if (license) {
+//     return `${renderLicenseLink(license)}${license}`
+//   } else {
+//     return '';
+//   }
+// }
+
+// TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
-  ${licenseBadge[data.license]}
+  return `
 
+# ${data.title}
+![GitHub license](https://img.shields.io/badge/Made%20by-%40${data.username}-green)
 ## Description
 ${data.description}
 
